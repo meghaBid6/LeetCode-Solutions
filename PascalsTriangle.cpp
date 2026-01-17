799d4b3c8cd079e4de351aa9b836910db55a4b13
meghaBid6 <meghabid06@gmail.com>
2026-01-17 09:52:12 +0530

    Add Pascal's Triangle solution in C++

---
+++ PascalsTriangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>val;
+        for(int i = 0; i < numRows; i++){
+            vector<int> row(i+1,1);
+            for(int j = 1; j < i; j++){
+             row[j] = val[i-1][j-1] + val[i-1][j];  
+            }
+            val.push_back(row);
+        }
+        return val;
+    }
+};
